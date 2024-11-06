da41d77a5e426b378050a1c10283bc994fa846e8
omeshapasan2 <omesha.20232170@iit.ac.lk>
2024-11-06 16:55:59 +0530

    new changes

---
+++ src/test/java/com/oopCw/EventTicketingSystem/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopCw.EventTicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
