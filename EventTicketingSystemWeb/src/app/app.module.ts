51de94af8bb93787f15c01feb9335b62de9ccca9
omeshapasan2 <omesha.20232170@iit.ac.lk>
2024-11-07 19:30:00 +0530

    added basic components

--- EventTicketingSystemWeb/src/app/app.module.ts
+++ EventTicketingSystemWeb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './basic/components/login/login.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
